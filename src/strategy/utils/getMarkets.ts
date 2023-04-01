6dafb00085599e05f3892599cfd3e85f908ac936
soheil <soheilrasekh555@gmail.com>
2023-04-01 17:02:34 +0330

    docs: update .env.example

--- src/strategy/utils/getMarkets.ts
+++ src/strategy/utils/getMarkets.ts
@@ -1,13 +1,10 @@
-import { Asset } from '@dydxprotocol/v3-client'
 import { client } from '../dydxClient'
 
 export async function getMarkets() {
     const { markets } = await client.public.getMarkets()
 
     const filteredMarkets = Object.entries(markets).filter(
-        ([_, marketInfo]) =>
-            marketInfo.status === 'ONLINE' &&
-            marketInfo.quoteAsset === Asset.USDC
+        ([_, marketInfo]) => marketInfo.status === 'ONLINE'
     )
 
     return Object.fromEntries(filteredMarkets)
