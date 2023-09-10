0bc23d197fae1910e5440a4a6bf801a1491433b4
soheil <soheilrasekh555@gmail.com>
2023-09-10 20:57:01 +0330

    getMarketsPrices method

--- apps/api/src/market-data/market-data.module.ts
+++ apps/api/src/market-data/market-data.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MarketDataService } from './market-data.service';
+import { DydxModule } from 'src/dydx/dydx.module';
 
 @Module({
   providers: [MarketDataService],
   exports: [MarketDataService],
+  imports: [DydxModule],
 })
 export class MarketDataModule {}
--- apps/api/src/market-data/market-data.service.ts
+++ apps/api/src/market-data/market-data.service.ts
@@ -1,14 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Config } from 'src/config';
 import { DydxService } from 'src/dydx/dydx.service';
+import {
+  CandleResolution,
+  CandleResponseObject,
+  Market,
+} from '@dydxprotocol/v3-client';
 
 @Injectable()
 export class MarketDataService {
-  constructor(private readonly dydx: DydxService) {}
+  constructor(
+    private readonly dydx: DydxService,
+    private readonly config: ConfigService<Config, true>,
+  ) {}
 
   async getTradeableMarkets() {
     const client = this.dydx.getPublicClient();
     const { markets } = await client.public.getMarkets();
 
-    return markets;
+    const onlineMarkets = Object.values(markets).filter(
+      (market) => market.status === 'ONLINE',
+    );
+
+    return onlineMarkets;
+  }
+
+  async getMarketPrices(market: Market) {
+    const client = this.dydx.getPublicClient();
+    const limit = this.config.get('CANDLE_LIMIT', { infer: true });
+    const resolution = this.config.get('CANDLE_RESOLUTION', {
+      infer: true,
+    }) as CandleResolution;
+
+    const { candles } = await client.public.getCandles({
+      market,
+      resolution,
+      limit,
+    });
+
+    if (candles.length !== limit) return [];
+
+    return candles;
+  }
+
+  async getMarketsPrices() {
+    const markets = await this.getTradeableMarkets();
+
+    const marketsPrices: Record<string, CandleResponseObject[]> = {};
+
+    for (const market of markets) {
+      const marketPrices = await this.getMarketPrices(market.market);
+      if (marketPrices.length === 0) continue;
+      marketsPrices[market.market] = marketPrices;
+    }
+
+    return marketsPrices;
+  }
+
+  async getCointegratedPairs() {
+    const marketsPrices = await this.getMarketsPrices();
+    const markets = Object.keys(marketsPrices);
+
+    const included: Record<string, boolean> = {};
+
+    for (const marketA of markets) {
+      for (const marketB of markets) {
+        if (marketA === marketB) continue;
+
+        const unique = [marketA, marketB].sort().join('-');
+        if (included[unique]) continue;
+
+        included[unique] = true;
+
+        const series1 = this.extractClosePrices(marketsPrices[marketA]);
+        const series2 = this.extractClosePrices(marketsPrices[marketB]);
+      }
+    }
+  }
+
+  extractClosePrices(candles: CandleResponseObject[]) {
+    return candles.map((candle) => Number(candle.close));
   }
 }
